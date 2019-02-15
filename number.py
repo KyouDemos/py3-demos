cf2c60c8ca8ba5ccd52db285277b2dacb8dc198e
kyou <wgoodq@foxmail.com>
2019-02-15 08:00:00 +0800

    pythone numbers

---
+++ number.py
@@ -0,0 +1,25 @@
+# 简单的四则运算
+print('123+123: ', 123+123)
+print('321-123: ', 321-123)
+print('111-222: ', 111-222)
+print('4*5: ', 4*5)
+# 注意输出的值是全精度的"
+print('3.2*7: ', 3.2*7)
+# 注意输出的值带有小数位"
+print('4/2:  ', 4/2)
+print('5//2: ', 5//2)   # 取整（向下取整）
+print('5%2:  ', 5%2)    # 取余
+
+# 高阶计算
+print('2**10: ', 2**10)
+print('|-4|', abs(-4))
+
+import math
+print('π: ', math.pi)
+print('e: ', math.e)
+print('√9: ', math.sqrt(9))
+
+# 随机数
+import random as rd
+print('random.random: ', rd.random)
+print('random..choice([1,2,3,4]): ', rd.choice([1,2,3,4]))
