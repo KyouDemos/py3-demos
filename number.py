7130ba28f144c91f10b033c3b59de5cced0d8952
kyou <wgoodq@foxmail.com>
2019-02-18 08:40:44 +0800

    小数、分数、集合

--- number.py
+++ number.py
@@ -36,10 +36,65 @@ print('π:', math.pi)
 print('e:', math.e)  # 自然底数
 print('√9:', math.sqrt(9))  # 平方根
 print('9 ** (1 / 2):', 9 ** (1 / 2))  # 平方根
-print('9 ** (1 / 3):', 9 ** (1 / 3))  # 立方根
+print('27 ** (1 / 3):', 27 ** (1 / 3))  # 立方根
 
 # 随机数
 import random as rd
 
 print('random.random:', rd.random)
 print('random.choice([1,2,3,4]):', rd.choice([1, 2, 3, 4]))
+
+# 小数运算
+import decimal
+
+# 对比一下两种计算结果的打印值
+print(decimal.Decimal(0.1) + decimal.Decimal(0.1) - decimal.Decimal(0.3))  # -0.09999999999999997779553950745
+print(decimal.Decimal('0.1') + decimal.Decimal('0.1') - decimal.Decimal('0.3'))  # -0.1(函数中需要传入字符串)
+
+# 设定全局进度
+decimal.getcontext().prec = 2
+print(decimal.Decimal(0.1) + decimal.Decimal(0.1) - decimal.Decimal(0.3))  # -0.10
+print(decimal.Decimal('0.1') + decimal.Decimal('0.1') - decimal.Decimal('0.3'))  # -0.1
+
+# 分数
+from fractions import Fraction
+
+print('Fraction(2, 3):', Fraction(2, 3))  # 函数内必须是数字
+
+# 分数运算
+a = Fraction(1, 2)
+b = Fraction(1, 3)
+print('a + b:', a + b)
+print('a - b:', a - b)
+print('a * b:', a * b)
+print('a / b:', a / b)
+
+# 分数、小数转换
+print("Fraction('0.6'):", Fraction('0.6'))  # 3/5(函数中需要传入字符串)
+print("Fraction(0.6):", Fraction(0.6))  # 5404319552844595/9007199254740992
+print(Fraction(str(1.2 * 4)))
+
+# 集合
+x = set('abcdef')
+y = set('bdmn')
+print('x:', x)
+print('y:', y)
+
+x.add('g')
+print("x.add('g'):", x)
+x.update('h')
+print("x.update('h'):", x)
+x.pop()
+print("x.pop():", x)  # 删除第一个元素
+x.remove('h')
+print("x.remove('h'):", x)  # 删除指定元素
+
+# 集合运算
+print("set('bc') in x:", set('bc') in x)
+print('x - y:', x - y)
+print('x & y:', x & y)
+print('x | y:', x | y)
+print('x ^ y:', x ^ y)
+
+# 集合解析
+print("{i*2 for i in x}:", {i * 2 for i in x})
